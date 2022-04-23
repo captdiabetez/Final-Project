68347146d6e5bd450bd3606248931648c7f7f566
John Hernandez <jkhernandez2@buffs.wtamu.edu>
2022-04-23 13:19:08 -0500

    Test

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AssetASP.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Final_Project
 {
@@ -24,6 +26,9 @@ namespace Final_Project
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<AssetDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("AssetContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
