7e422ec77509dd506a445e33398017bf5ad231f0
John Hernandez <jkhernandez2@buffs.wtamu.edu>
2022-04-23 14:37:13 -0500

    Test

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -5,20 +5,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using AssetASP.Models;
 
 namespace Final_Project.Pages
 {
     public class PrivacyModel : PageModel
     {
+        private readonly AssetDbContext _context; // Repaces "db" variable
+        public List<Asset> Assets {get; set;}
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
         public void OnGet()
         {
+            Assets = _context.Assets.ToList();
         }
     }
 }
